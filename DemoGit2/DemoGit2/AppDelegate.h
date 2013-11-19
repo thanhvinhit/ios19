99c832b9baece7aa8c783996d446cdf3ca747e07
thanhvinhit <thanhvinhit@icloud>
2013-11-19 19:58:54 +0700

    Git 2

---
+++ DemoGit2/DemoGit2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DemoGit2
+//
+//  Created by Do Thanh Vinh on 11/19/13.
+//  Copyright (c) 2013 ThanhVinh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DemoGit2/DemoGit2/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DemoGit2
+//
+//  Created by Do Thanh Vinh on 11/19/13.
+//  Copyright (c) 2013 ThanhVinh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
